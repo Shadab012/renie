1f7a4aea094f4b4336c0fc8a7049fa7c7d55d8eb
AbdulSamad676 <abdulsamad31650@gmail.com>
2024-05-27 23:18:29 +0500

    Hero section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../assets/css/home.css';
 import Arrow from '../assets/images/arrow.png';
+import Circle from '../assets/images/circle.png';
+import RoundedIcon from '../assets/images/roundIcon.png';
 function Home() {
   return (
-    <div className='heroSection flex flex-col justify-between py-10  items-center  mt-10'>
-      <div className='heroTitle flex flex-col gap-10 mt-10'>
+    <div className='heroSection flex flex-col justify-between py-5 md:py-10 gap-5 md:gap-0 items-center  mt-10'>
+      <div className='mobileCircle  flex justify-start md:hidden w-full px-10'>
+        <img className='bigCircle' src={Circle} alt='' />
+        <img className='smallCircle' src={Circle} alt='' />
+      </div>
+      <div className='heroTitle flex flex-col gap-5 md:gap-10  md:mt-10'>
         <div className='heroTitleText'>
           {' '}
           <span className='transform'>Transform</span> <br />
@@ -16,6 +22,9 @@ function Home() {
         <div className='flex justify-center md:mt-5'>
           <button className='heroButton text-white'>See how</button>
         </div>
+      </div>
+      <div className='mobileRounded flex justify-end md:hidden w-full px-10'>
+        <img className='roundedIcon' src={RoundedIcon} alt='' />
       </div>
       <div className='arrowSection mb-10'>
         <img className='arrowImg' src={Arrow} alt='' />
